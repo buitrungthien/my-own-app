632f70337e57d2aaaa42468e4baecc46ba616058
Thien <thienbt95@gmail.com>
2019-10-16 17:17:59 +0700

    minor changes

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -3,10 +3,15 @@ import './styles.scss';
 import icon from '../../images/sprite.svg';
 
 const search = props => {
+    
+    const onSearchIconClick = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <form className='search'>
-            <input className='search__input' placeholder='Search'/>
-            <button className='search__icon'>
+            <input className='search__input' placeholder='Search' />
+            <button className='search__icon' onClick={onSearchIconClick}>
                 <svg><use href={`${icon}#icon-search`}></use></svg>
             </button>
         </form>
